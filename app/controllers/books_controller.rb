6b3f84ac196d17da091595ab096e56f0dd917d9e
Val <val@home.fr>
2019-10-11 14:49:32 +0200

    Added udapte and destroy functions to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,25 @@ class BooksController < ApplicationController
   def index
     @mes_livres = Book.all
   end
+
   def create
     Book.create title: params[:title]
     flash[:notice] = "Le livre a bien été ajouté"
     redirect_to "/books"
   end
+
   def show
     @book = Book.find(params[:id])
+  end
+
+  def update
+    Book.find(params[:id]).update title: params[:title]
+    redirect_to "/books/#{params[:id]}"
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    flash[:notice] = "Le livre a bien été supprimé"
+    redirect_to "/books"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get  'books'   => 'books#index'
   # post 'books'    => 'books#create'
   # get  'books/:id' => 'books#show'
-  resources :books, only: [:show, :index, :create]
+  resources :books, only: [:show, :index, :create, :update, :destroy]
   get  'home'    => 'pages#home'
   get  'mdr'     => 'pages#mdr'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
